93a6a087539a9b4b3a7624f3b4c076d2f465f99b
damatebitii <damatebitia@gmail.com>
2025-01-21 17:41:19 +0400

    init

---
+++ Assets/script/fold.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fold : MonoBehaviour
+{
+    
+    
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        transform.Translate(2, 2, 2);
+    }
+}
